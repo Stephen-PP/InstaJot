190cbec4d7a7939c10429e8b57d98578c004825d
Stephen-PP <ppstephen2016@gmail.com>
2024-01-02 17:39:27 -0500

    changed server port to use env var

--- api/server/server.go
+++ api/server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/stephen-pp/instajot/database"
@@ -17,7 +18,16 @@ func CreateServer() {
 	fmt.Println("Registering routes...")
 	RegisterRoutes(app)
 
-	app.Listen(":3000")
+	// Determine port to use
+	port := ""
+	if os.Getenv("PORT") != "" {
+		port = fmt.Sprintf(":%s", os.Getenv("PORT"))
+	} else {
+		port = ":3000"
+	}
+
+	fmt.Printf("Listening on port %s...\n", port)
+	app.Listen(port)
 
 	// Close the database connection when the server shuts down
 	app.Hooks().OnShutdown(func() error {
