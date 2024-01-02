7e844b67152d045267544e9e4ca3193e0ca8ecb6
Stephen-PP <ppstephen2016@gmail.com>
2024-01-02 17:36:25 -0500

    added database connection

--- api/database/database.go
+++ api/database/database.go
@@ -1,17 +1,69 @@
 package database
 
 import (
-	"log"
 	"database/sql"
+	"log"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func CreateDatabase() {
+func CreateDatabase() *sql.DB {
 	db, err := sql.Open("sqlite3", "./instajot.db")
 	if err != nil {
 		log.Println("An error occurred opening the database connection.")
 		panic(err)
 	}
 
-	db.
+	createNotesDatabase(db)
+	createUserDatabase(db)
+
+	return db
+}
+
+func createUserDatabase(db *sql.DB) {
+	// Create the users table if it doesn't exist
+	stmt, err := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS Users (
+		UserId TEXT PRIMARY KEY,
+		Username TEXT,
+		Email TEXT,
+		Password TEXT,
+		CreatedAt TIMESTAMP default (strftime('%s', 'now'))
+	)`)
+
+	// Make sure we prepared the table successfully
+	if err != nil {
+		log.Println("An error occurred while preparing the users table.")
+		panic(err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Println("An error occurred creating the users table.")
+		panic(err)
+	}
+}
+
+func createNotesDatabase(db *sql.DB) {
+	// Create the notes table if it doesn't exist
+	stmt, err := db.Prepare(`
+	CREATE TABLE IF NOT EXISTS Notes (
+		NoteId TEXT,
+		HistoryId TEXT PRIMARY KEY,
+		UserId INTEGER FOREIGN KEY REFERENCES Users(UserId),
+		Content BLOB,
+		CreatedAt TIMESTAMP default (strftime('%s', 'now'))
+	)`)
+
+	// Make sure we prepared the table successfully
+	if err != nil {
+		log.Println("An error occurred while preparing the notes table.")
+		panic(err)
+	}
+
+	_, err = stmt.Exec()
+	if err != nil {
+		log.Println("An error occurred creating the notes table.")
+		panic(err)
+	}
 }
--- api/server/server.go
+++ api/server/server.go
@@ -1,15 +1,27 @@
-package api
+package server
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/stephen-pp/instajot/database"
 )
 
 func CreateServer() {
+	fmt.Println("Creating SQLite database...")
+	db := database.CreateDatabase()
+
+	fmt.Println("Creating server...")
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
+	fmt.Println("Registering routes...")
+	RegisterRoutes(app)
 
 	app.Listen(":3000")
+
+	// Close the database connection when the server shuts down
+	app.Hooks().OnShutdown(func() error {
+		db.Close()
+		return nil
+	})
 }
